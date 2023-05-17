bef2de23e9b1dfd0b2987146a6af06661bca94df
makenzie9838 <86166064+makenzie9838@users.noreply.github.com>
2023-05-17 19:01:00 -0400

    Updated comments and added dynamic to-do list

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const myImage = document.querySelector("img");
 
+//switches image on each click with the relevant alert
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/IMG-0906.jpg") {
@@ -12,8 +13,13 @@ myImage.onclick = () => {
 };
 
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
 
+//click event for button to set username
+myButton.onclick = () => {
+  setUserName();
+};
+
+//function to set username
 function setUserName() {
     const myName = prompt("Please enter your name.");
     if (!myName) {
@@ -24,6 +30,7 @@ function setUserName() {
     }
   }
 
+//sets username or welcomes user depending on what is in local storage
 if (!localStorage.getItem("name")) {
     setUserName();
   } else {
@@ -31,6 +38,36 @@ if (!localStorage.getItem("name")) {
     alert(`Welcome back, ${storedName}!`);
   }
 
-myButton.onclick = () => {
-    setUserName();
-};
+//initializes variables for unordered list, list name, description and selects the html file
+const list = document.createElement('ul');
+const listName = document.createElement('h3');
+const listDescription = document.createElement('p');
+const html = document.querySelector('html');
+
+//names the list and describes how the list is used
+listName.textContent = 'Dynamic To-Do List';
+listDescription.textContent = 'Click anywhere to add a new task to the list. Click an existing task to change it.'
+
+//adds those variables to the document
+document.body.appendChild(listName);
+document.body.appendChild(listDescription);
+document.body.appendChild(list);
+
+//click events to add or change items in the list
+html.onclick = () => {
+  const listItem = document.createElement('li');
+  const listContent = prompt('What do you have to do today?');
+  if (listContent) {
+    listItem.textContent = listContent;
+    list.appendChild(listItem);
+  }
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('What else do you have to do today?');
+    if (listContent) {
+      this.textContent = listContent;
+    }
+  }
+}
+
